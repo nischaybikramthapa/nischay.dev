6a0233260f2a86a000b6c48df808e354afca14d7
nischaybikramthapa <nischaybikramthapa14@gmail.com>
2025-10-25 11:36:21 +1100

    updated profile

--- data/careerTimeline.ts
+++ data/careerTimeline.ts
@@ -1,9 +1,23 @@
 const careersData = [
+  {
+      title: 'AI Architect, Australia Post',
+      time: 'Jan 2024 - Apr 2025',
+      description: `Responsible for designing and implementing AI strategies and architecture 
+      to enhance operational efficiency and customer experience across various business units.
+      `,
+    },
+  {
+      title: 'Solution Architect, Ignite Data Solutions',
+      time: 'Jan 2024 - Apr 2025',
+      description: `I’ve led enterprise data and AI modernisation across 
+      the 5 utilities companies aligning with regulatory obligations, value chain priorities, and sustainability goals.
+      `,
+    },
     {
       title: 'Lead Data Consultant, The Data Foundry',
       time: 'November 2021 - Present',
-      description: `Driving strategy and execution for automating data pipelines and
-      machine learning models at scale by translating business use cases
+      description: `Responsible for strategy and execution for automating data pipelines and
+      machine learning solutions at scale by translating business use cases
       into innovative solutions for customers.
       `,
     },
